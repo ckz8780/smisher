e46ebae1ed8248d33affab38ba4a3714777f4021
Christopher Zielinski <ckz8780@gmail.com>
2021-01-22 21:55:53 -0500

    added admin and fixed models

--- shortcodes/admin.py
+++ shortcodes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ShortCode, ShortCodeLease
 
 # Register your models here.
+admin.site.register(ShortCode)
+admin.site.register(ShortCodeLease)
--- shortcodes/models.py
+++ shortcodes/models.py
@@ -4,7 +4,7 @@ from customers.models import Customer
 # Create your models here.
 class ShortCode(models.Model):
   shortcode = models.PositiveBigIntegerField()
-  description = models.CharField(max_length=10000, default=True, null=True)
+  description = models.CharField(max_length=10000, null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   is_available = models.BooleanField(default=True)
   available_on = models.DateTimeField(blank=True, null=True)
@@ -13,7 +13,7 @@ class ShortCode(models.Model):
 class ShortCodeLease(models.Model):
   leased_to = models.ForeignKey(Customer, on_delete=models.CASCADE)
   shortcode = models.OneToOneField(ShortCode, on_delete=models.CASCADE)
-  description = models.CharField(max_length=10000, default=True, null=True)
+  description = models.CharField(max_length=10000, null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   expires_at = models.DateTimeField(auto_now_add=True)
   is_active = models.BooleanField(default=False)
